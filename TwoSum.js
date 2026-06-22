71816d83720b62f73f25df0dc62c61b5cc9494e1
Prince <princeglobiz@gmail.com>
2026-06-22 10:51:14 +0530

    Add Two Sum solution

---
+++ TwoSum.js
@@ -0,0 +1,18 @@
+const twoSum = (nums, target) => {
+    const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const first = nums[i];
+        const second = target - first;
+
+        if (map.has(second)) {
+            return [map.get(second), i];
+        }
+
+        map.set(first, i);
+    }
+
+    return [];
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
